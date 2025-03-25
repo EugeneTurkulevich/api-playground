f8c9df6f81ec4742a7871eb5f18f73f8e06cdd32
EugeneTurkulevich <eugene.turkulevich@gmail.com>
2025-03-25 11:59:56 +0200

    LS

--- playground_app.py
+++ playground_app.py
@@ -36,7 +36,10 @@ def set_local_storage_js(set_key, value_to_set):
 
 components.html(get_local_storage_js('temperature', "0.3"), height=0)
 components.html(get_local_storage_js('max_tokens', "50"), height=0)
-components.html(get_local_storage_js('openai_key', ""), height=0)
+components.html(get_local_storage_js('openai_model', "gpt-3.5-turbo"), height=0)
+components.html(get_local_storage_js('openai_system_prompt', ""), height=0)
+components.html(get_local_storage_js('openai_user_prompt', ""), height=0)
+components.html(get_local_storage_js('openai_api_key', ""), height=0)
 components.html(get_local_storage_js('grok_key', ""), height=0)
 
 st.sidebar.title("AI API Playground")
@@ -76,10 +79,12 @@ with tab1:
     openai_user_prompt = st.text_area("OpenAI User Prompt", height=150)
 
     if st.button("Send to OpenAI"):
-        # st.session_state['temperature'] = temperature
-        # st.session_state['max_tokens'] = max_tokens
         components.html(set_local_storage_js("temperature", temperature), height=0)
         components.html(set_local_storage_js("max_tokens", max_tokens), height=0)
+        components.html(set_local_storage_js("openai_model", openai_selected_model), height=0)
+        components.html(set_local_storage_js("openai_system_prompt", openai_system_prompt), height=0)
+        components.html(set_local_storage_js("openai_user_prompt", openai_user_prompt), height=0)
+        components.html(set_local_storage_js("openai_api_key", openai_api_key), height=0)
         if not openai_api_key:
             st.error("Please provide a valid API key.")
         else:
